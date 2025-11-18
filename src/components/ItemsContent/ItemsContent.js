c54d08429d543a214fd080da9eb5f52f6307b9b2
totorz76 <victor76.zammit@gmail.com>
2025-11-18 10:13:33 +0100

    first commit "html"

---
+++ src/components/ItemsContent/ItemsContent.js
@@ -0,0 +1,12 @@
+const itemContent = [
+ {title :"Ripped Skinny Jeans", price : "$24.99", img : "../../assets/img/jeans1.jpg"},
+ {title :"Mega Ripped Jeans", price : "$19.99", img : "../../assets/img/jeans2.jpg"},
+ {title :"Washed Skinny Jeans", price :"$20.50", img : "../../assets/img/jeans3.jpg"},
+ {title :"Vintage Skinny Jeans", price :"$14.99", img : "../../assets/img/jeans4.jpg"},
+ {title :"Washed Skinny Jeans", price :"$20.50", img : "../../assets/img/jeans3.jpg"},
+ {title :"Vintage Skinny Jeans", price :"$14.99", img : "../../assets/img/jeans4.jpg"},
+ {title :"Ripped Skinny Jeans", price : "$24.99", img : "../../assets/img/jeans1.jpg"},
+ {title :"Mega Ripped Jeans", price : "$19.99", img : "../../assets/img/jeans2.jpg"},
+]
+
+export {itemContent}
---
+++ src/components/MenuContent/MenuContent.js
@@ -0,0 +1,13 @@
+const menuContent = [
+    { title: "Shirts", icon: "", subMenu: [{ title: "Skinny", icon: "Relaxed" }, { title: "Bootcut", icon: "" }, { title: "Straight", icon: "" }] },
+    { title: "Dresses", icon: "", subMenu: [{ title: "Skinny", icon: "Relaxed" }, { title: "Bootcut", icon: "" }, { title: "Straight", icon: "" }] },
+    { title: "Jeans", icon: "", subMenu: [{ title: "Skinny", icon: "Relaxed" }, { title: "Bootcut", icon: "" }, { title: "Straight", icon: "" }] },
+    { title: "Jackets", icon: "", subMenu: [{ title: "Skinny", icon: "Relaxed" }, { title: "Bootcut", icon: "" }, { title: "Straight", icon: "" }] },
+    { title: "Gymwear", icon: "", subMenu: [{ title: "Skinny", icon: "Relaxed" }, { title: "Bootcut", icon: "" }, { title: "Straight", icon: "" }] },
+    { title: "Blazers", icon: "", subMenu: [{ title: "Skinny", icon: "Relaxed" }, { title: "Bootcut", icon: "" }, { title: "Straight", icon: "" }] },
+    { title: "Shoes", icon: "", subMenu: [{ title: "Skinny", icon: "Relaxed" }, { title: "Bootcut", icon: "" }, { title: "Straight", icon: "" }] },
+    { title: "Contact", icon: "", subMenu: [] },
+    { title: "Newsletter", icon: "", subMenu: [] },
+    { title: "Subscribe", icon: "", subMenu: [] }
+]
+export {menuContent}
